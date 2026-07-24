33e1b4ad441d19383baed0d26cf98c5ad0e6ef46
Abdo <nopointstudios27@gmail.com>
2026-07-24 17:48:47 +0300

    some polish to the scene

--- Assets/_LoadNExplode/_Scripts/UI/Billboard.cs
+++ Assets/_LoadNExplode/_Scripts/UI/Billboard.cs
@@ -5,7 +5,7 @@ public class BillboardYAxis : MonoBehaviour
     void LateUpdate()
     {
         Vector3 direction = Camera.main.transform.position - transform.position;
-        direction.y = 0; // Flatten to horizontal plane
+        // direction.y = 0; // Flatten to horizontal plane
         direction.Normalize();
 
         if (direction != Vector3.zero)
